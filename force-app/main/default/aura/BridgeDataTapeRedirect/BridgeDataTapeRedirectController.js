643a69bc10de8d7a1b4b75c6b06177aadeb2c802
Liron de Castro <60845154+lirondco@users.noreply.github.com>
2022-09-15 10:58:09 -0700

    bridge redirect updated

--- force-app/main/default/aura/BridgeDataTapeRedirect/BridgeDataTapeRedirectController.js
+++ force-app/main/default/aura/BridgeDataTapeRedirect/BridgeDataTapeRedirectController.js
@@ -1,31 +1,17 @@
 ({
   redirect: function (component, event, helper) {
     var recordId = component.get("v.recordId");
-    var action = component.get("c.getRecordTypeName");
-    action.setParams({
-      i: recordId
-    });
-    action.setCallback(this, function (response) {
-      let state = response.getState();
-      if ("SUCCESS" === state) {
-        let returnVal = response.getReturnValue();
-        console.log("--record id--");
-        console.log(recordId);
-				let componentName = returnVal == "Single_Asset_Bridge_Loan" ?  "c:SabFciDataTape" : "c:BridgeDataTape";
-        var evt = $A.get("e.force:navigateToComponent");
-        evt.setParams({
-          componentDef: componentName,
-          componentAttributes: {
-            recordId: recordId,
-            isRedirect: true
-          }
-        });
-
-        evt.fire();
-      } else {
-        console.log(response.getError());
+    const record = component.get("v.dealRecord");
+    const isFciSab = record.Record_Type_Name__c == "Single_Asset_Bridge_Loan" && record.Servicer_Name__c == "FCI";
+    let componentName = isFciSab ?  "c:SabFciDataTape" : "c:BridgeDataTape";
+    var evt = $A.get("e.force:navigateToComponent");
+    evt.setParams({
+      componentDef: componentName,
+      componentAttributes: {
+        recordId: recordId,
+        isRedirect: true
       }
     });
-    $A.enqueueAction(action);
-  }
+
+    evt.fire();  }
 });
