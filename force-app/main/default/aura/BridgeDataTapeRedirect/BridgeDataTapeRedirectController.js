f79208ee8b9c56357153a209c8aae2e5845041d0
Liron de Castro <60845154+lirondco@users.noreply.github.com>
2022-09-19 15:45:00 -0700

    completed sab boarding tape components

--- force-app/main/default/aura/BridgeDataTapeRedirect/BridgeDataTapeRedirectController.js
+++ force-app/main/default/aura/BridgeDataTapeRedirect/BridgeDataTapeRedirectController.js
@@ -1,17 +1,18 @@
 ({
-  redirect: function (component, event, helper) {
-    var recordId = component.get("v.recordId");
-    const record = component.get("v.dealRecord");
-    const isFciSab = record.Record_Type_Name__c == "Single_Asset_Bridge_Loan" && record.Servicer_Name__c == "FCI";
-    let componentName = isFciSab ?  "c:SabFciDataTape" : "c:BridgeDataTape";
-    var evt = $A.get("e.force:navigateToComponent");
-    evt.setParams({
-      componentDef: componentName,
-      componentAttributes: {
-        recordId: recordId,
-        isRedirect: true
-      }
-    });
+	redirect : function(component, event, helper) {
+		var recordId = component.get('v.recordId');
+		console.log('--record id--');
+		console.log(recordId);
+		var evt = $A.get("e.force:navigateToComponent");
+		evt.setParams({
+			componentDef : 'c:BridgeDataTape',
+			componentAttributes : {
+				recordId: recordId,
+				isRedirect: true
+			}
+		});
 
-    evt.fire();  }
-});
+		evt.fire();
+
+	}
+})
