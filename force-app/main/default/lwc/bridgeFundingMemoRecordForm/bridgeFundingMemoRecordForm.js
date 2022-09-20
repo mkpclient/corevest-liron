9650cee99d108c542e09e97b9880d3297b679763
Liron de Castro <60845154+lirondco@users.noreply.github.com>
2022-09-20 15:58:32 -0700

    ticket scope changed, removed LWC and replaced with PDF VF Page
    rebased

--- force-app/main/default/lwc/bridgeFundingMemoRecordForm/bridgeFundingMemoRecordForm.js
+++
@@ -1,9 +0,0 @@
-import { api, LightningElement } from 'lwc';
-
-export default class BridgeFundingMemoRecordForm extends LightningElement {
-  @api recordId;
-  @api recordData;
-  @api formData;
-
-  
-}
