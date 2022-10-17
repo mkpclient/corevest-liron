a0fc3641882607ece257e16772aa4287e8daecbd
Liron de Castro <60845154+lirondco@users.noreply.github.com>
2022-10-17 10:36:03 -0700

    updated and added new email templates

--- force-app/main/default/lwc/newAdvanceApproval/newAdvanceApproval.js
+++ force-app/main/default/lwc/newAdvanceApproval/newAdvanceApproval.js
@@ -2,15 +2,24 @@ import { api, LightningElement, wire } from "lwc";
 import { CloseActionScreenEvent } from "lightning/actions";
 import upsertRecord from "@salesforce/apex/lightning_Controller.upsertRecord";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
-import { getFieldValue, getRecord, getRecordNotifyChange } from 'lightning/uiRecordApi';
+import {
+  getFieldValue,
+  getRecord,
+  getRecordNotifyChange
+} from "lightning/uiRecordApi";
 
 import ADVANCE_APPROVAL from "@salesforce/schema/Advance__c.Batch_Approval__c";
 import APPROVAL_STATUS from "@salesforce/schema/Advance__c.Batch_Approval__r.Approval_Status__c";
+import query from "@salesforce/apex/lightning_Util.query";
 
 export default class NewAdvanceApproval extends LightningElement {
   @api recordId;
-  isLoading = false;
-  @wire(getRecord, { recordId: "$recordId", fields: [ADVANCE_APPROVAL, APPROVAL_STATUS] })
+  isLoading = true;
+  hasFundingApprovalPackage = false;
+  @wire(getRecord, {
+    recordId: "$recordId",
+    fields: [ADVANCE_APPROVAL, APPROVAL_STATUS]
+  })
   advanceRecord;
 
   get approvalId() {
@@ -22,7 +31,45 @@ export default class NewAdvanceApproval extends LightningElement {
   }
 
   get hasApproval() {
-    return this.approvalId && !["Recalled", "Rejected"].includes(this.approvalStatus);
+    return (
+      this.approvalId && !["Recalled", "Rejected"].includes(this.approvalStatus)
+    );
+  }
+
+  renderedCallback() {
+    if(this.recordId && this.isLoading && !this.hasFundingApprovalPackage) {
+      this.checkIfFileExists();
+    }
+  }
+
+  async checkIfFileExists() {
+    let queryString = `SELECT Id, ContentVersion_Id__c FROM Deal_Document__c WHERE Document_Type__c='Funding Approval Package' AND Advance__c='${this.recordId}' AND Is_Deleted__c = FALSE AND ContentVersion_Id__c != null Order By CreatedDate Desc`;
+    console.log(queryString);
+    const dealDocs = await query({ queryString });
+
+    if (dealDocs.length > 0) {
+      const cvIds = [];
+
+      dealDocs.forEach((d) => {
+        if (d.ContentVersion_Id__c && d.ContentVersion_Id__c.length > 0) {
+          cvIds.push(d.ContentVersion_Id__c);
+        }
+      });
+
+      if (cvIds.length > 0) {
+        queryString = `SELECT Id FROM ContentVersion WHERE Id IN ('${cvIds.join(
+          "','"
+        )}')`;
+
+        console.log(queryString);
+
+        const cvs = await query({ queryString });
+
+        this.hasFundingApprovalPackage = cvs.length > 0;
+      }
+    }
+
+    this.isLoading = false;
   }
 
   handleClick(evt) {
@@ -51,11 +98,11 @@ export default class NewAdvanceApproval extends LightningElement {
       Id: recId,
       AttachmentPosted__c: true,
       sobjectType: "Batch_Approval__c"
-    }
+    };
 
     await upsertRecord({ record: batch });
 
-    getRecordNotifyChange([{recordId: this.recordId}, {recordId: recId}]);
+    getRecordNotifyChange([{ recordId: this.recordId }, { recordId: recId }]);
 
     this.dispatchEvent(new CloseActionScreenEvent());
     this.dispatchEvent(
